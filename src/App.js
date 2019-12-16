be7870f1a110069addaa15e5ee28290799f312b5
LMTrain <layconmm@msn.com>
2019-12-15 23:03:54 -0600

    Used useReducer instead of using Redux library

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-import React, {useContext } from 'react';
+import React, {useContext, useReducer } from 'react';
 import {UserContext} from './index';
+
+const initialState = {
+  count: 0
+}
+
+function reducer(state, action) {
+  switch(action.type) {
+    case "increment":
+      return {
+        count: state.count + 1
+      };
+    case "decrement":
+      return {
+        count: state.count - 1
+      }
+    case "reset":
+      return initialState
+    default:
+      return initialState
+  }
+}
 
 //PROPS DRILLING
 // export default function App(props)
@@ -12,13 +33,37 @@ import {UserContext} from './index';
 // }
 
 export default function App() {
-  const value = useContext(UserContext)
-  return <div>
-    Hello, {value}
+  const [state, dispatch] = useReducer(reducer, initialState)
+  const value = useContext(UserContext);
+
+  return (
+  <div>
+   
     {/* <UserContext.Consumer>
       {value => <div>Hello, {value}</div>}
     </UserContext.Consumer> */}
-    </div>;
+    count: {state.count}
+    <button 
+      className="border m-2 p-1"
+      onClick={() => dispatch({ type: "increment"})}
+    >
+      Increment
+    </button>
+    <button 
+      className="border m-2 p-1"
+      onClick={() => dispatch({ type: "decrement"})}
+    >
+      Decrement
+    </button>
+    <button 
+      className="border m-2 p-1"
+      onClick={() => dispatch({ type: "reset"})}
+    >
+      Reset
+    </button>
+  </div>
+  
+  );
   
 }
 

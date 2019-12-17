86a2167554d7aa29dcbf9c7a4f2a31a1bac71cd9
LMTrain <layconmm@msn.com>
2019-12-16 23:02:54 -0600

    Combine useContext hook and useReducer hook to make intial App

--- src/index.js
+++ src/index.js
@@ -1,21 +1,48 @@
-import React from 'react';
+import React, {useContext, useReducer } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import * as serviceWorker from './serviceWorker';
+import TodoContext from './context';
+import todoReducer from './reducer';
+
+import TodoList from './components/TodoList';
+
+
+
+const App = () => {
+    const initialState = useContext(TodoContext)
+    const [state, dispatch] = useReducer(todoReducer, initialState)
+
+    return (
+        <TodoContext.Provider value={{ state, dispatch}}>
+            <TodoList />
+       
+        </TodoContext.Provider>
+    )
+    
+}
 
 //<== PROPS DRILLING ==>
 // const username= "Laycon"
 // ReactDOM.render(<App username={username}/>, document.getElementById('root'));
 
-export const  UserContext = React.createContext()
-const username = "Laycon"
+// export const  UserContext = React.createContext()
+// const username = "Laycon"
+
+// ReactDOM.render(
+//     <UserContext.Provider value={username}>
+//         <App />
+//     </UserContext.Provider>
+
+// , document.getElementById('root'));
 
 ReactDOM.render(
-    <UserContext.Provider value={username}>
-        <App />
-    </UserContext.Provider>
+    
+    <App />,
 
-, document.getElementById('root'));
+
+document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.

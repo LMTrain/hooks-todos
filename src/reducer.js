65a8439a21b335d38d329f8754385b5ae340d2b8
LMTrain <layconmm@msn.com>
2020-01-14 02:21:16 -0600

    Improve App in adding empty Todo

--- src/reducer.js
+++ src/reducer.js
@@ -4,6 +4,12 @@ import uuidv4 from 'uuid/v4';
 export default function reducer(state, action) {
     switch(action.type) {
         case "ADD_TODO":
+            if (!action.payload) {
+                return state;
+            }
+            if (state.todos.findIndex(t => t.text === action.payload) > -1) {
+                return state;
+            }
             const newTodo = {
                 id: uuidv4(),
                 text: action.payload,
@@ -30,6 +36,12 @@ export default function reducer(state, action) {
         };
 
         case "UPDATE_TODO":
+            if (!action.payload) {
+                return state;
+            }
+            if (state.todos.findIndex(t => t.text === action.payload) > -1) {
+                return state;
+            }
             const updatedTodo = { ...state.currentTodo, text: action.payload }
             const updatedTodoIndex = state.todos.findIndex(
                 t => t.id === state.currentTodo.id
@@ -47,8 +59,11 @@ export default function reducer(state, action) {
 
         case "REMOVE_TODO":
             const filteredTodos = state.todos.filter(t => t.id !== action.payload.id)
+            const isRemovedTodo = state.currentTodo.id === action.payload.id ? {} :
+            state.currentTodo;
         return {
             ...state,
+            currentTodo: isRemovedTodo,
             todos: filteredTodos
         };
         default:
